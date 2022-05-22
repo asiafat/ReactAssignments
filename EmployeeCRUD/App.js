3b102e29949522fbbf62fe49b20e09709fd2ea5a
Shaik Asia Fathima <102518778+asiafat@users.noreply.github.com>
2022-05-22 21:36:03 +0530

    Add files via upload

---
+++ EmployeeCRUD/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import EmpCrud from './components/EmpCrud';
+import CreateEmployee from './components/CreateEmployee';
+
+function App() {
+  return (
+    <div className="App">
+      <EmpCrud/>
+      <CreateEmployee/>
+    
+    </div>
+
+  );
+}
+
+export default App;
